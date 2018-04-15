efc8d9ce7edc70aad7008a05d9f3c62fd974cd3b
Unknown <96jaslat@gmail.com>
2018-04-14 22:19:14 -0600

    Perfil de usuario - Activity creado y diseñado v2

--- Fluxme/app/src/main/java/me/flux/fluxme/Business/PerfilActivity.java
+++ Fluxme/app/src/main/java/me/flux/fluxme/Business/PerfilActivity.java
@@ -1,13 +1,25 @@
 package me.flux.fluxme.Business;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class PerfilActivity extends AppCompatActivity {
+import me.flux.fluxme.R;
+
+public class PerfilActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        initToolbar();
+
+        Usuario_Singleton u = Usuario_Singleton.getInstance();
+        EditText nombrePerfilEdit = findViewById(R.id.PerfilNombreEditText);
+        nombrePerfilEdit.setText(u.getNombre());
+
+        EditText usuarioPerfilEdit = findViewById(R.id.PerfilUsuarioEditText);
+        usuarioPerfilEdit.setText(u.getAuth_token());
     }
 }
